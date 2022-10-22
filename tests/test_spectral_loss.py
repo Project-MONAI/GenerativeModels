c4bcce125e0eb48d089b66e8c7e63e767ee372c2
Warvito <ianonimato@hotmail.com>
2022-10-22 16:22:18 +0100

    Add tests (#15)

--- tests/test_spectral_loss.py
+++ tests/test_spectral_loss.py
@@ -11,13 +11,76 @@
 
 import unittest
 
+import numpy as np
 import torch
+from parameterized import parameterized
 from tests.utils import test_script_save
 
 from generative.losses import JukeboxLoss
 
+TEST_CASES = [
+    [
+        {
+            "spatial_dims": 2,
+        },
+        {
+            "input": torch.tensor([[[[1.0, 1.0, 0.0], [0.0, 0.0, 1.0]], [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]]]]),
+            "target": torch.tensor([[[[1.0, 0.0, 0.0], [0.0, 0.0, 1.0]], [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]]]]),
+        },
+        0.070648,
+    ],
+    [
+        {
+            "spatial_dims": 2,
+            "reduction": "sum",
+        },
+        {
+            "input": torch.tensor([[[[1.0, 1.0, 0.0], [0.0, 0.0, 1.0]], [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]]]]),
+            "target": torch.tensor([[[[1.0, 0.0, 0.0], [0.0, 0.0, 1.0]], [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]]]]),
+        },
+        0.8478,
+    ],
+    [
+        {
+            "spatial_dims": 3,
+        },
+        {
+            "input": torch.tensor(
+                [
+                    [
+                        [[[1.0, 1.0, 0.0], [0.0, 0.0, 1.0]], [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]]],
+                        [[[1.0, 1.0, 0.0], [0.0, 0.0, 1.0]], [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]]],
+                    ]
+                ]
+            ),
+            "target": torch.tensor(
+                [
+                    [
+                        [[[1.0, 0.0, 0.0], [0.0, 0.0, 1.0]], [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]]],
+                        [[[1.0, 1.0, 0.0], [0.0, 0.0, 1.0]], [[1.0, 0.0, 1.0], [0.0, 1.0, 0.0]]],
+                    ]
+                ]
+            ),
+        },
+        0.03838,
+    ],
+]
+
 
 class TestJukeboxLoss(unittest.TestCase):
+    @parameterized.expand(TEST_CASES)
+    def test_results(self, input_param, input_data, expected_val):
+        results = JukeboxLoss(**input_param).forward(**input_data)
+        np.testing.assert_allclose(results.detach().cpu().numpy(), expected_val, rtol=1e-4)
+
+    def test_2D_shape(self):
+        results = JukeboxLoss(spatial_dims=2, reduction="none").forward(**TEST_CASES[0][1])
+        self.assertEqual(results.shape, (1, 2, 2, 3))
+
+    def test_3D_shape(self):
+        results = JukeboxLoss(spatial_dims=3, reduction="none").forward(**TEST_CASES[2][1])
+        self.assertEqual(results.shape, (1, 2, 2, 2, 3))
+
     def test_script(self):
         loss = JukeboxLoss(spatial_dims=2)
         test_input = torch.ones(2, 1, 8, 8)
