dde1841520838ddf1f92ae1b75f445ad84d3f097
JessyD <jejedafflon@gmail.com>
2022-11-04 19:50:49 -0400

    Improve sections of the tutorial (#48)

--- tutorials/generative/autoencoderkl/tutorial_autoencoderkl.py
+++ tutorials/generative/autoencoderkl/tutorial_autoencoderkl.py
@@ -1,9 +1,35 @@
 # # AutoencoderKL
 #
-# This demo is a toy example of how to use MONAI's AutoencoderKL. It is based on
-# Based on
-# https://github.com/Project-MONAI/tutorials/blob/main/2d_registration/registration_mednist.ipynb
-# https://github.com/Project-MONAI/tutorials/blob/main/2d_classification/mednist_tutorial.ipynb
+# This demo is a toy example of how to use MONAI's AutoencoderKL. In particular, it uses
+# the Autoencoder with a Kullback-Leibler regularisation as implemented by Rombach et. al [1].
+#
+# [1] Rombach et. al - ["High-Resolution Image Synthesis with Latent Diffusion Models"](https://arxiv.org/pdf/2112.10752.pdf)
+#
+#
+#
+# This tutorial was based on:
+#
+# [Registration Mednist](https://github.com/Project-MONAI/tutorials/blob/main/2d_registration/registration_mednist.ipynb)
+#
+# [Mednist Tutorial](https://github.com/Project-MONAI/tutorials/blob/main/2d_classification/mednist_tutorial.ipynb)
+#
+#
+#
+
+# +
+# TODO: Add buttom with "Open with Colab"
+# -
+
+# ## Set up environment using Colab
+
+# !pip install -q "monai-weekly==1.1.dev2239"
+# !pip install -q matplotlib
+# !pip install -q  torch>=1.7
+# !pip install numpy>=1.17
+# !pip install tqdm
+# %matplotlib inline
+
+# ## Setup imports
 
 # +
 import os
@@ -23,8 +49,12 @@ from tqdm import tqdm
 from generative.networks.nets import AutoencoderKL
 
 print_config()
-set_determinism(42)
 # -
+
+# for reproducibility purposes set a seed
+set_determinism(42)
+
+# ## Setup a data directory and download dataset
 
 # Specify a `MONAI_DATA_DIRECTORY` variable, where the data will be downloaded. If not
 # specified a temporary directory will be used.
@@ -35,6 +65,8 @@ directory = os.environ.get("MONAI_DATA_DIRECTORY")
 root_dir = tempfile.mkdtemp() if directory is None else directory
 print(root_dir)
 # -
+
+# ### Download the training set
 
 train_data = MedNISTDataset(root_dir=root_dir, section="training", download=True, seed=0)
 train_datalist = [{"image": item["image"]} for item in train_data.data if item["class_name"] == "Hand"]
@@ -59,11 +91,16 @@ train_transforms = transforms.Compose(
 train_ds = Dataset(data=train_datalist, transform=train_transforms)
 train_loader = DataLoader(train_ds, batch_size=64, shuffle=True, num_workers=4)
 
+# ### Visualise examples from the training set
+
 # Plot 3 examples from the training set
 check_data = first(train_loader)
 fig, ax = plt.subplots(nrows=1, ncols=3)
 for image_n in range(3):
     ax[image_n].imshow(check_data["image"][image_n, 0, :, :], cmap="gray")
+plt.axis("off")
+
+# ### Download the validation set
 
 val_data = MedNISTDataset(root_dir=root_dir, section="validation", download=True, seed=0)
 val_datalist = [{"image": item["image"]} for item in train_data.data if item["class_name"] == "Hand"]
@@ -77,8 +114,10 @@ val_transforms = transforms.Compose(
 val_ds = Dataset(data=val_datalist, transform=val_transforms)
 val_loader = DataLoader(val_ds, batch_size=64, shuffle=True, num_workers=4)
 
-device = torch.device("Using cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)
+# ## Define the network
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using {device}")
 model = AutoencoderKL(
     spatial_dims=2,
     in_channels=1,
@@ -91,11 +130,19 @@ model = AutoencoderKL(
     norm_num_groups=16,
     with_attention=False,
 )
+model.to(device)
 
+# ## Model Training
+
+# +
 kl_weight = 1e-6
 optimizer = torch.optim.Adam(model.parameters(), 1e-5)
-n_epochs = 20
-val_interval = 2
+n_epochs = 50
+val_interval = 5
+epoch_loss_list = []
+val_epoch_loss_list = []
+intermediary_images = []
+
 for epoch in range(n_epochs):
     model.train()
     epoch_loss = 0
@@ -124,17 +171,20 @@ for epoch in range(n_epochs):
                 "loss": epoch_loss / (step + 1),
             }
         )
+    epoch_loss_list.append(epoch_loss / (step + 1))
 
     if (epoch + 1) % val_interval == 0:
         model.eval()
-        val_step = 0
         val_loss = 0
         with torch.no_grad():
-            for batch in val_loader:
-                val_step += 1
+            for val_step, batch in enumerate(val_loader, start=1):
                 images = batch["image"].to(device)
                 optimizer.zero_grad(set_to_none=True)
                 reconstruction, z_mu, z_sigma = model(images)
+                # get the first sammple from the first validation batch for visualisation
+                # purposes
+                if val_step == 1:
+                    intermediary_images.append(reconstruction[0, 0])
 
                 l1_loss = F.l1_loss(reconstruction.float(), images.float())
 
@@ -148,5 +198,33 @@ for epoch in range(n_epochs):
                 val_loss += loss.item()
 
         val_loss /= val_step
+        val_epoch_loss_list.append(val_loss)
+
         print(f"epoch {epoch + 1} val loss: {val_loss:.4f}")
 progress_bar.close()
+
+# -
+
+# ## Evaluate the training
+# ### Visualise the loss
+
+plt.figure()
+plt.plot(np.linspace(1, n_epochs, n_epochs), epoch_loss_list, label="Train")
+plt.plot(np.linspace(val_interval, n_epochs, int(n_epochs / val_interval)), val_epoch_loss_list, label="Test")
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend()
+plt.tight_layout()
+plt.show()
+plt.close()
+
+# ### Visualise some reconstruction images
+
+# plot the reconstructions at each of the evaluation steps
+reconstructions = torch.concat(intermediary_images, dim=-1)
+plt.figure()
+plt.imshow(reconstructions.cpu(), vmin=0, vmax=1, cmap="gray")
+plt.tight_layout()
+plt.axis("off")
+plt.show()
+plt.close()
