391a5f320bf2e69e1b9bd64727751de438c9c652
JessyD <jejedafflon@gmail.com>
2022-11-14 08:38:51 -0500

    Initial tutorial draft

---
+++ tutorials/generative/2d_ldm/2d_ldm_tutorial.py
@@ -0,0 +1,146 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.1
+# ---
+
+# # 2D Latent Diffusion Model
+
+# +
+# TODO: Add buttom with "Open with Colab"
+# -
+
+# ## Set up environment using Colab
+#
+
+# !python -c "import monai" || pip install -q "monai-weekly[tqdm]"
+# !python -c "import matplotlib" || pip install -q matplotlib
+# %matplotlib inline
+
+# ## Set up imports
+
+# +
+import os
+import tempfile
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from monai import transforms
+from monai.apps import MedNISTDataset
+from monai.config import print_config
+from monai.data import DataLoader, Dataset
+from monai.utils import first, set_determinism
+
+from generative.networks.nets import AutoencoderKL, DiffusionModelUNet, LatentDiffusionModel
+from generative.schedulers import DDPMScheduler
+
+print_config()
+# -
+
+# for reproducibility purposes set a seed
+set_determinism(42)
+
+# ## Setup a data directory and download dataset
+# Specify a MONAI_DATA_DIRECTORY variable, where the data will be downloaded. If not specified a temporary directory will be used.
+
+directory = os.environ.get("MONAI_DATA_DIRECTORY")
+root_dir = tempfile.mkdtemp() if directory is None else directory
+print(root_dir)
+
+# ## Download the training set
+
+train_data = MedNISTDataset(root_dir=root_dir, section="training", download=True, seed=0)
+train_datalist = [{"image": item["image"]} for item in train_data.data if item["class_name"] == "Hand"]
+image_size = 64
+train_transforms = transforms.Compose(
+    [
+        transforms.LoadImaged(keys=["image"]),
+        transforms.EnsureChannelFirstd(keys=["image"]),
+        transforms.ScaleIntensityRanged(keys=["image"], a_min=0.0, a_max=255.0, b_min=0.0, b_max=1.0, clip=True),
+        # TODO: Change transformations
+        transforms.RandAffined(
+            keys=["image"],
+            rotate_range=[(-np.pi / 36, np.pi / 36), (-np.pi / 36, np.pi / 36)],
+            translate_range=[(-1, 1), (-1, 1)],
+            scale_range=[(-0.05, 0.05), (-0.05, 0.05)],
+            spatial_size=[image_size, image_size],
+            padding_mode="zeros",
+            prob=0.5,
+        ),
+    ]
+)
+train_ds = Dataset(data=train_datalist, transform=train_transforms)
+train_loader = DataLoader(train_ds, batch_size=64, shuffle=True, num_workers=4)
+
+# ## Visualise examples from the training set
+
+# Plot 3 examples from the training set
+check_data = first(train_loader)
+fig, ax = plt.subplots(nrows=1, ncols=3)
+for image_n in range(3):
+    ax[image_n].imshow(check_data["image"][image_n, 0, :, :], cmap="gray")
+    ax[image_n].axis("off")
+# TODO: remove path
+plt.savefig("/project/tutorials/generative/2d_ldm/hand_examples.png")
+
+# ## Download the validation set
+
+val_data = MedNISTDataset(root_dir=root_dir, section="validation", download=True, seed=0)
+val_datalist = [{"image": item["image"]} for item in train_data.data if item["class_name"] == "Hand"]
+val_transforms = transforms.Compose(
+    [
+        transforms.LoadImaged(keys=["image"]),
+        transforms.EnsureChannelFirstd(keys=["image"]),
+        transforms.ScaleIntensityRanged(keys=["image"], a_min=0.0, a_max=255.0, b_min=0.0, b_max=1.0, clip=True),
+    ]
+)
+val_ds = Dataset(data=val_datalist, transform=val_transforms)
+val_loader = DataLoader(val_ds, batch_size=64, shuffle=True, num_workers=4)
+
+# ## Define the network
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using {device}")
+
+# +
+stage1_model = AutoencoderKL(
+    spatial_dims=2,
+    in_channels=1,
+    out_channels=1,
+    num_channels=64,
+    latent_channels=8,
+    ch_mult=(1, 2, 3),
+    num_res_blocks=1,
+    norm_num_groups=16,
+    attention_levels=(False, False, True),
+)
+
+unet = DiffusionModelUNet(
+    spatial_dims=2,
+    in_channels=3,
+    out_channels=3,
+    num_res_blocks=1,
+    attention_resolutions=[4, 2],
+    channel_mult=[1, 2, 2],
+    model_channels=64,
+    # TODO: play with this number
+    num_heads=1,
+)
+
+scheduler = DDPMScheduler(
+    num_train_timesteps=1000,
+    beta_schedule="linear",
+    beta_start=0.0015,
+    beta_end=0.0195,
+)
+
+model = LatentDiffusionModel(first_stage=stage1_model, unet=unet, scheduler=scheduler)
+
+model = model.to(device)
