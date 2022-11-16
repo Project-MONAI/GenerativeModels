d8f1e0492acc74e1b1a0e94c38e47ce876326159
Mark Graham <markgraham539@gmail.com>
2022-11-16 16:14:02 -0600

    Adds diffusion sampling inferer

---
+++ generative/inferers/inferer.py
@@ -0,0 +1,61 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any, Callable
+
+import torch
+from monai.inferers import Inferer
+from tqdm import tqdm
+
+
+class DiffusionSamplingInferer(Inferer):
+    """
+    DiffusionSamplingInferer takes a trained diffusion model and a scheduler and produces a sample.
+    """
+
+    def __init__(self) -> None:
+        Inferer.__init__(self)
+
+    def __call__(
+        self,
+        input_noise: torch.Tensor,
+        diffusion_model: Callable[..., torch.Tensor],
+        scheduler: Callable[..., torch.Tensor],
+        save_intermediates: bool = False,
+        intermediate_steps: int = 100,
+        *args: Any,
+        **kwargs: Any
+    ):
+        """Unified callable function API of Inferers.
+        Args:
+            input_noise: random noise, of the same shape as the desired sample.
+            diffusion_model: model to sample from.
+            scheduler: diffusion scheduler.
+            save_intermediates: whether to return intermediates along the sampling change
+            intermediate_steps: if save_intermediates is True, saves every n steps
+            args: optional args to be passed to ``network``.
+            kwargs: optional keyword args to be passed to ``network``.
+        """
+        image = input_noise.clone()
+        progress_bar = tqdm(scheduler.timesteps)
+        intermediates = []
+        for t in progress_bar:
+            # 1. predict noise model_output
+            with torch.no_grad():
+                model_output = diffusion_model(image, torch.Tensor((t,)).to(input_noise.device))
+            # 2. compute previous image: x_t -> x_t-1
+            image, _ = scheduler.step(model_output, t, image)
+            if save_intermediates and t % intermediate_steps == 0:
+                intermediates.append(image)
+        if save_intermediates:
+            return image, intermediates
+        else:
+            return image
