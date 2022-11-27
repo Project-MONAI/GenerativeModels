7fa697041ed1a0e12cad089a30fb45050e4ab632
PedroFerreiradaCosta <pedro.hpf.costa@gmail.com>
2022-11-27 16:27:01 +0000

    prehook  fix
    Signed-off-by: PedroFerreiradaCosta <pedro.hpf.costa@gmail.com>

--- generative/metrics/ms_ssim.py
+++ generative/metrics/ms_ssim.py
@@ -8,16 +8,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import List, Optional, Union, Tuple
+from typing import List, Optional, Union
 
 import torch
 import torch.nn.functional as F
-
-
 from monai.metrics import SSIMMetric
-from monai.utils import MetricReduction
 from monai.metrics.regression import RegressionMetric
-from monai.utils.type_conversion import convert_to_dst_type
+from monai.utils import MetricReduction
 
 
 class MS_SSIM(RegressionMetric):
@@ -26,19 +23,24 @@ class MS_SSIM(RegressionMetric):
 
     [1] Wang, Z., Simoncelli, E.P. and Bovik, A.C., 2003, November.
             Multiscale structural similarity for image quality assessment.
-            In The Thrity-Seventh Asilomar Conference on Signals, Systems & Computers, 2003 (Vol. 2, pp. 1398-1402). Ieee.
+            In The Thrity-Seventh Asilomar Conference on Signals, Systems
+            & Computers, 2003 (Vol. 2, pp. 1398-1402). Ieee.
 
     Args:
         data_range: synamic range of the data
         win_size: gaussian weighting window size
         k1: stability constant used in the luminance denominator
         k2: stability constant used in the contrast denominator
-        spatial_dims: if 2, input shape is expected to be (B,C,W,H); if 3, it is expected to be (B,C,W,H,D)
-        weights: parameters for image similarity and contrast sensitivity at different resolution scores.
+        spatial_dims: if 2, input shape is expected to be (B,C,W,H);
+                      if 3, it is expected to be (B,C,W,H,D)
+        weights: parameters for image similarity and contrast sensitivity
+                 at different resolution scores.
         reduction: {``"none"``, ``"mean"``, ``"sum"``}
-            Specifies the reduction to apply to the output. Defaults to ``"mean"``.
+            Specifies the reduction to apply to the output.
+            Defaults to ``"mean"``.
             - ``"none"``: no reduction will be applied.
-            - ``"mean"``: the sum of the output will be divided by the number of elements in the output.
+            - ``"mean"``: the sum of the output will be divided by the number
+             of elements in the output.
             - ``"sum"``: the output will be summed.
     """
 
@@ -72,33 +74,43 @@ class MS_SSIM(RegressionMetric):
             self.spatial_dims,
         )
 
-    def _compute_metric(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    def _compute_metric(
+                        self,
+                        x: torch.Tensor,
+                        y: torch.Tensor
+                        ) -> torch.Tensor:
         """
         Args:
-            x: first sample (e.g., the reference image). Its shape is (B,C,W,H) for 2D data and (B,C,W,H,D) for 3D.
-                A fastMRI sample should use the 2D format with C being the number of slices.
-            y: second sample (e.g., the reconstructed image). It has similar shape as x
+            x: first sample (e.g., the reference image). Its shape is
+                (B,C,W,H) for 2D data and (B,C,W,H,D) for 3D.
+                A fastMRI sample should use the 2D format with C being
+                the number of slices.
+            y: second sample (e.g., the reconstructed image). It has similar
+               shape as x
 
         Returns:
             ms_ssim_value
         """
 
         if not x.shape == y.shape:
-            raise ValueError(f"Input images should have the same dimensions, but got {x.shape} and {y.shape}.")
+            raise ValueError(f"Input images should have the same dimensions, \
+             but got {x.shape} and {y.shape}.")
 
         for d in range(len(x.shape) - 1, 1, -1):
             x = x.squeeze(dim=d)
             y = y.squeeze(dim=d)
 
         if not x.type() == y.type():
-            raise ValueError(f"Input images should have the same dtype, but got {x.type()} and {y.type()}.")
+            raise ValueError(f"Input images should have the same dtype, but \
+                got {x.type()} and {y.type()}.")
 
         if len(x.shape) == 4:
             avg_pool = F.avg_pool2d
         elif len(x.shape) == 5:
             avg_pool = F.avg_pool3d
         else:
-            raise ValueError(f"Input images should be 4-d or 5-d tensors, but got {x.shape}")
+            raise ValueError(f"Input images should be 4-d or 5-d tensors, but \
+                got {x.shape}")
 
         if self.weights is None:
             # as per Ref 1 - Sec 3.2.
@@ -110,12 +122,14 @@ class MS_SSIM(RegressionMetric):
         for idx, shape_size in enumerate(x.shape[2:]):
             if shape_size % divisible_by != 0:
                 raise ValueError(
-                    f"Image size needs to be divisible by {divisible_by} but dimension {idx + 2} has size {shape_size}"
+                    f"Image size needs to be divisible by {divisible_by} but \
+                        dimension {idx + 2} has size {shape_size}"
                 )
 
             if shape_size < bigger_than:
                 raise ValueError(
-                    f"Image size should be larger than {bigger_than} due to the {len(self.weights) - 1} downsamplings in MS-SSIM."
+                    f"Image size should be larger than {bigger_than} due to \
+                        the {len(self.weights) - 1} downsamplings in MS-SSIM."
                 )
 
         levels = self.weights.shape[0]
@@ -130,8 +144,10 @@ class MS_SSIM(RegressionMetric):
                 y = avg_pool(y, kernel_size=2, padding=padding)
 
         ssim = torch.relu(ssim)  # (batch, 1)
-        mcs_and_ssim = torch.stack(mcs_list + [ssim], dim=0)  # (level, batch, 1)
-        ms_ssim = torch.prod(mcs_and_ssim ** self.weights.view(-1, 1, 1), dim=0)
+        # (level, batch, 1)
+        mcs_and_ssim = torch.stack(mcs_list + [ssim], dim=0)
+        ms_ssim = torch.prod(mcs_and_ssim ** self.weights.view(-1, 1, 1),
+                             dim=0)
 
         if self.reduction == MetricReduction.MEAN.value:
             ms_ssim = ms_ssim.mean()
