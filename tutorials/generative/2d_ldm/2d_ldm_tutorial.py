aece4db05fe89bbae17ce370493fa6c64bea7372
Jessica Dafflon <JessyD@users.noreply.github.com>
2022-12-03 05:23:55 -0500

    Create Latent Diffusion Model tutorial using 2D data (#54)
    * Initial tutorial draft
    
    * Import LatentDiffusionModel (#54)
    
    * [WIP] Add training step (#54)
    
    * Update training (#54)
    
    * [WIP] Add 2d-ldm inference (#54)
    
    * Remove the LatentDiffusionModel class (#54)
    
    * Update notebook (#54)
    
    * [WIP] Implement corrections to the Latent Diffusion Model (#54)
    
    * Use DiffusionInferer and tune hyperparameters
    
    * [WIP] Add perceptual and adversarial loss
    
    * Use reformatted Unet and fix typo
    
    * Use newest version of the PatchDiscriminator (#54)
    
    * Fine tune hyperparemeters (#54)
    
    * Transform set up environment back to code (#54)

---
+++ tutorials/generative/2d_ldm/2d_ldm_tutorial.py
@@ -0,0 +1,423 @@
+# # 2D Latent Diffusion Model
+
+# +
+# TODO: Add buttom with "Open with Colab"
+# -
+
+# ## Set up environment using Colab
+#
+
+# !python -c "import monai" || pip install -q "monai-weekly[tqdm]"
+# !python -c "import matplotlib" || pip install -q matplotlib
+# %matplotlib inline
+
+# ## Set up imports
+
+# +
+import os
+import shutil
+import tempfile
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn.functional as F
+from monai import transforms
+from monai.apps import MedNISTDataset
+from monai.config import print_config
+from monai.data import DataLoader, Dataset
+from monai.networks.layers import Act
+from monai.utils import first, set_determinism
+from tqdm import tqdm
+
+from generative.inferers import DiffusionInferer
+from generative.losses.adversarial_loss import PatchAdversarialLoss
+from generative.losses.perceptual import PerceptualLoss
+from generative.networks.nets import AutoencoderKL, DiffusionModelUNet, PatchDiscriminator
+from generative.schedulers import DDPMScheduler
+
+print_config()
+# -
+
+# for reproducibility purposes set a seed
+set_determinism(42)
+
+# ## Setup a data directory and download dataset
+# Specify a MONAI_DATA_DIRECTORY variable, where the data will be downloaded. If not specified a temporary directory will be used.
+
+directory = os.environ.get("MONAI_DATA_DIRECTORY")
+root_dir = tempfile.mkdtemp() if directory is None else directory
+print(root_dir)
+
+# ## Download the training set
+
+train_data = MedNISTDataset(root_dir=root_dir, section="training", download=True, seed=0)
+train_datalist = [{"image": item["image"]} for item in train_data.data if item["class_name"] == "Hand"]
+image_size = 64
+train_transforms = transforms.Compose(
+    [
+        transforms.LoadImaged(keys=["image"]),
+        transforms.EnsureChannelFirstd(keys=["image"]),
+        transforms.ScaleIntensityRanged(keys=["image"], a_min=0.0, a_max=255.0, b_min=0.0, b_max=1.0, clip=True),
+        # TODO: Change transformations
+        transforms.RandAffined(
+            keys=["image"],
+            rotate_range=[(-np.pi / 36, np.pi / 36), (-np.pi / 36, np.pi / 36)],
+            translate_range=[(-1, 1), (-1, 1)],
+            scale_range=[(-0.05, 0.05), (-0.05, 0.05)],
+            spatial_size=[image_size, image_size],
+            padding_mode="zeros",
+            prob=0.5,
+        ),
+    ]
+)
+train_ds = Dataset(data=train_datalist, transform=train_transforms)
+train_loader = DataLoader(train_ds, batch_size=64, shuffle=True, num_workers=4)
+
+# ## Visualise examples from the training set
+
+# Plot 3 examples from the training set
+check_data = first(train_loader)
+fig, ax = plt.subplots(nrows=1, ncols=3)
+for image_n in range(3):
+    ax[image_n].imshow(check_data["image"][image_n, 0, :, :], cmap="gray")
+    ax[image_n].axis("off")
+
+
+# ## Download the validation set
+
+val_data = MedNISTDataset(root_dir=root_dir, section="validation", download=True, seed=0)
+val_datalist = [{"image": item["image"]} for item in train_data.data if item["class_name"] == "Hand"]
+val_transforms = transforms.Compose(
+    [
+        transforms.LoadImaged(keys=["image"]),
+        transforms.EnsureChannelFirstd(keys=["image"]),
+        transforms.ScaleIntensityRanged(keys=["image"], a_min=0.0, a_max=255.0, b_min=0.0, b_max=1.0, clip=True),
+    ]
+)
+val_ds = Dataset(data=val_datalist, transform=val_transforms)
+val_loader = DataLoader(val_ds, batch_size=64, shuffle=True, num_workers=4)
+
+# ## Define the network
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Using {device}")
+
+autoencoderkl = AutoencoderKL(
+    spatial_dims=2,
+    in_channels=1,
+    out_channels=1,
+    num_channels=64,
+    latent_channels=3,
+    ch_mult=(1, 2, 2),
+    num_res_blocks=1,
+    norm_num_groups=32,
+    attention_levels=(False, False, True),
+)
+autoencoderkl = autoencoderkl.to(device)
+
+
+# +
+unet = DiffusionModelUNet(
+    spatial_dims=2,
+    in_channels=3,
+    out_channels=3,
+    num_res_blocks=1,
+    num_channels=(128, 256, 256),
+    num_head_channels=256,
+)
+
+scheduler = DDPMScheduler(
+    num_train_timesteps=1000,
+    beta_schedule="linear",
+    beta_start=0.0015,
+    beta_end=0.0195,
+)
+
+inferer = DiffusionInferer(scheduler)
+
+discriminator = PatchDiscriminator(
+    spatial_dims=2,
+    num_layers_d=3,
+    num_channels=32,
+    in_channels=1,
+    out_channels=1,
+    kernel_size=4,
+    activation=(Act.LEAKYRELU, {"negative_slope": 0.2}),
+    norm="BATCH",
+    bias=False,
+    padding=1,
+)
+discriminator.to(device)
+
+# +
+perceptual_loss = PerceptualLoss(spatial_dims=2, network_type="alex")
+perceptual_loss.to(device)
+perceptual_weight = 0.001
+
+adv_loss = PatchAdversarialLoss(criterion="least_squares")
+adv_weight = 0.01
+
+optimizer_g = torch.optim.Adam(autoencoderkl.parameters(), lr=1e-4)
+optimizer_d = torch.optim.Adam(discriminator.parameters(), lr=5e-4)
+# -
+
+scaler_g = torch.cuda.amp.GradScaler()
+scaler_d = torch.cuda.amp.GradScaler()
+
+# ## Train AutoencoderKL
+
+# It takes about ~60 min to train the model.
+
+# +
+kl_weight = 1e-6
+n_epochs = 100
+val_interval = 6
+autoencoder_warm_up_n_epochs = 10
+
+epoch_recon_loss_list = []
+epoch_gen_loss_list = []
+epoch_disc_loss_list = []
+val_recon_epoch_loss_list = []
+intermediary_images = []
+n_example_images = 4
+
+for epoch in range(n_epochs):
+    autoencoderkl.train()
+    discriminator.train()
+    epoch_loss = 0
+    gen_epoch_loss = 0
+    disc_epoch_loss = 0
+    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), ncols=110)
+    progress_bar.set_description(f"Epoch {epoch}")
+    for step, batch in progress_bar:
+        images = batch["image"].to(device)
+        optimizer_g.zero_grad(set_to_none=True)
+
+        reconstruction, z_mu, z_sigma = autoencoderkl(images)
+
+        recons_loss = F.l1_loss(reconstruction.float(), images.float())
+        p_loss = perceptual_loss(reconstruction.float(), images.float())
+        kl_loss = 0.5 * torch.sum(z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2)) - 1, dim=[1, 2, 3])
+        kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+        loss_g = recons_loss + (kl_weight * kl_loss) + (perceptual_weight * p_loss)
+
+        if epoch > autoencoder_warm_up_n_epochs:
+            logits_fake = discriminator(reconstruction.contiguous().float())[-1]
+            generator_loss = adv_loss(logits_fake, target_is_real=True, for_discriminator=False)
+            loss_g += adv_weight * generator_loss
+
+        loss_g.backward()
+        optimizer_g.step()
+
+        if epoch > autoencoder_warm_up_n_epochs:
+            optimizer_d.zero_grad(set_to_none=True)
+
+            logits_fake = discriminator(reconstruction.contiguous().detach())[-1]
+            loss_d_fake = adv_loss(logits_fake, target_is_real=False, for_discriminator=True)
+            logits_real = discriminator(images.contiguous().detach())[-1]
+            loss_d_real = adv_loss(logits_real, target_is_real=True, for_discriminator=True)
+            discriminator_loss = (loss_d_fake + loss_d_real) * 0.5
+
+            loss_d = adv_weight * discriminator_loss
+
+            loss_d.backward()
+            optimizer_d.step()
+
+        epoch_loss += recons_loss.item()
+        if epoch > autoencoder_warm_up_n_epochs:
+            gen_epoch_loss += generator_loss.item()
+            disc_epoch_loss += discriminator_loss.item()
+
+        progress_bar.set_postfix(
+            {
+                "recons_loss": epoch_loss / (step + 1),
+                "gen_loss": gen_epoch_loss / (step + 1),
+                "disc_loss": disc_epoch_loss / (step + 1),
+            }
+        )
+    epoch_recon_loss_list.append(epoch_loss / (step + 1))
+    epoch_gen_loss_list.append(gen_epoch_loss / (step + 1))
+    epoch_disc_loss_list.append(disc_epoch_loss / (step + 1))
+
+    if (epoch + 1) % val_interval == 0:
+        autoencoderkl.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for val_step, batch in enumerate(val_loader, start=1):
+                images = batch["image"].to(device)
+                optimizer_g.zero_grad(set_to_none=True)
+
+                reconstruction, z_mu, z_sigma = autoencoderkl(images)
+                # Get the first sammple from the first validation batch for visualisation
+                # purposes
+                if val_step == 1:
+                    intermediary_images.append(reconstruction[:n_example_images, 0])
+
+                recons_loss = F.l1_loss(images.float(), reconstruction.float())
+
+                val_loss += recons_loss.item()
+
+        val_loss /= val_step
+        val_recon_epoch_loss_list.append(val_loss)
+        print(f"epoch {epoch + 1} val loss: {val_loss:.4f}")
+progress_bar.close()
+
+# -
+
+# ### Visualise the results from the autoencoderKL
+
+# Plot last 5 evaluations
+val_samples = np.linspace(n_epochs, val_interval, int(n_epochs / val_interval))
+fig, ax = plt.subplots(nrows=5, ncols=1, sharey=True)
+for image_n in range(5):
+    reconstructions = torch.reshape(intermediary_images[image_n], (image_size * n_example_images, image_size)).T
+    ax[image_n].imshow(reconstructions.cpu(), cmap="gray")
+    ax[image_n].set_xticks([])
+    ax[image_n].set_yticks([])
+    ax[image_n].set_ylabel(f"Epoch {val_samples[image_n]:.0f}")
+
+# ## Train Diffusion Model
+
+# It takes about ~80 min to train the model.
+
+# +
+optimizer = torch.optim.Adam(unet.parameters(), lr=1e-4)
+
+unet = unet.to(device)
+n_epochs = 200
+val_interval = 40
+epoch_loss_list = []
+val_epoch_loss_list = []
+
+for epoch in range(n_epochs):
+    unet.train()
+    autoencoderkl.eval()
+    epoch_loss = 0
+    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), ncols=70)
+    progress_bar.set_description(f"Epoch {epoch}")
+    for step, batch in progress_bar:
+        images = batch["image"].to(device)
+        optimizer.zero_grad(set_to_none=True)
+
+        z_mu, z_sigma = autoencoderkl.encode(images)
+        z = autoencoderkl.sampling(z_mu, z_sigma)
+
+        noise = torch.randn_like(z).to(device)
+        noise_pred = inferer(inputs=z, diffusion_model=unet, noise=noise)
+        loss = F.mse_loss(noise_pred.float(), noise.float())
+
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+
+        progress_bar.set_postfix(
+            {
+                "loss": epoch_loss / (step + 1),
+            }
+        )
+    epoch_loss_list.append(epoch_loss / (step + 1))
+
+    if (epoch + 1) % val_interval == 0:
+        unet.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for val_step, batch in enumerate(val_loader, start=1):
+                images = batch["image"].to(device)
+                optimizer.zero_grad(set_to_none=True)
+
+                z_mu, z_sigma = autoencoderkl.encode(images)
+                z = autoencoderkl.sampling(z_mu, z_sigma)
+
+                noise = torch.randn_like(z).to(device)
+                noise_pred = inferer(inputs=z, diffusion_model=unet, noise=noise)
+
+                loss = F.mse_loss(noise_pred.float(), noise.float())
+
+                val_loss += loss.item()
+        val_loss /= val_step
+        val_epoch_loss_list.append(val_loss)
+        print(f"Epoch {epoch} val loss: {val_loss:.4f}")
+
+        # Sampling image during training
+        z = torch.randn((1, 3, 16, 16))
+        z = z.to(device)
+        scheduler.set_timesteps(num_inference_steps=1000)
+        for t in tqdm(scheduler.timesteps, ncols=70):
+            # 1. predict noise model_output
+            with torch.no_grad():
+                model_output = unet(z, torch.Tensor((t,)).to(device))
+
+                # 2. compute previous image: x_t -> x_t-1
+                z, _ = scheduler.step(model_output, t, z)
+
+        with torch.no_grad():
+            decoded = autoencoderkl.decode(z)
+        plt.figure(figsize=(2, 2))
+        plt.style.use("default")
+        plt.imshow(decoded[0, 0].cpu(), vmin=0, vmax=1, cmap="gray")
+        plt.tight_layout()
+        plt.axis("off")
+        plt.show()
+progress_bar.close()
+
+# -
+
+# ### Plotting sampling example
+
+# +
+unet.eval()
+image = torch.randn((1, 1, 64, 64))
+image = image.to(device)
+scheduler.set_timesteps(num_inference_steps=1000)
+
+with torch.no_grad():
+
+    z_mu, z_sigma = autoencoderkl.encode(image)
+    z = autoencoderkl.sampling(z_mu, z_sigma)
+
+    noise = torch.randn_like(z).to(device)
+    image, intermediates = inferer.sample(
+        input_noise=z, diffusion_model=unet, scheduler=scheduler, save_intermediates=True, intermediate_steps=100
+    )
+
+
+# -
+
+# Invert the autoencoderKL model
+decoded_images = []
+for image in intermediates:
+    with torch.no_grad():
+        decoded = autoencoderkl.decode(image)
+        decoded_images.append(decoded)
+plt.figure(figsize=(10, 12))
+chain = torch.cat(decoded_images, dim=-1)
+plt.style.use("default")
+plt.imshow(chain[0, 0].cpu(), vmin=0, vmax=1, cmap="gray")
+plt.tight_layout()
+plt.axis("off")
+
+
+# ## Plot learning curves
+plt.figure()
+plt.title("Learning Curves", fontsize=20)
+plt.plot(np.linspace(1, n_epochs, n_epochs), epoch_loss_list, linewidth=2.0, label="Train")
+plt.plot(
+    np.linspace(val_interval, n_epochs, int(n_epochs / val_interval)),
+    val_epoch_loss_list,
+    linewidth=2.0,
+    label="Validation",
+)
+plt.yticks(fontsize=12)
+plt.xticks(fontsize=12)
+plt.xlabel("Epochs", fontsize=16)
+plt.ylabel("Loss", fontsize=16)
+plt.legend(prop={"size": 14})
+
+
+# +
+### Clean-up data directory
+# -
+
+if directory is None:
+    shutil.rmtree(root_dir)
