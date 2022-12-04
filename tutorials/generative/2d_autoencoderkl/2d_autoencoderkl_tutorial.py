89fb74852457e9bb7cc5b36f3e5bca6d5493a2e0
JessyD <jejedafflon@gmail.com>
2022-12-04 15:08:39 -0500

    Add perceptual and adversarial loss

--- tutorials/generative/2d_autoencoderkl/2d_autoencoderkl_tutorial.py
+++ tutorials/generative/2d_autoencoderkl/2d_autoencoderkl_tutorial.py
@@ -27,10 +27,13 @@
 # !pip install -q einops
 # %matplotlib inline
 
+# %cd /mnt_homes/home4T7/jdafflon/GenerativeModels
+
 # ## Setup imports
 
 # +
 import os
+import shutil
 import tempfile
 
 import matplotlib.pyplot as plt
@@ -41,10 +44,13 @@ from monai import transforms
 from monai.apps import MedNISTDataset
 from monai.config import print_config
 from monai.data import DataLoader, Dataset
+from monai.networks.layers import Act
 from monai.utils import first, set_determinism
 from tqdm import tqdm
 
-from generative.networks.nets import AutoencoderKL
+from generative.losses.adversarial_loss import PatchAdversarialLoss
+from generative.losses.perceptual import PerceptualLoss
+from generative.networks.nets import AutoencoderKL, PatchDiscriminator
 
 print_config()
 # -
@@ -112,60 +118,121 @@ val_loader = DataLoader(val_ds, batch_size=64, shuffle=True, num_workers=4)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using {device}")
+
+# +
 model = AutoencoderKL(
     spatial_dims=2,
     in_channels=1,
     out_channels=1,
     num_channels=64,
-    latent_channels=8,
-    ch_mult=(1, 2, 3),
+    latent_channels=3,
+    ch_mult=(1, 2, 2),
     num_res_blocks=1,
-    norm_num_groups=16,
+    norm_num_groups=32,
     attention_levels=(False, False, True),
 )
-model.to(device)
+model = model.to(device)
+
+discriminator = PatchDiscriminator(
+    spatial_dims=2,
+    num_layers_d=3,
+    num_channels=32,
+    in_channels=1,
+    out_channels=1,
+    kernel_size=4,
+    activation=(Act.LEAKYRELU, {"negative_slope": 0.2}),
+    norm="BATCH",
+    bias=False,
+    padding=1,
+)
+discriminator.to(device)
+
+# +
+perceptual_loss = PerceptualLoss(spatial_dims=2, network_type="alex")
+perceptual_loss.to(device)
+perceptual_weight = 0.001
+
+adv_loss = PatchAdversarialLoss(criterion="least_squares")
+adv_weight = 0.01
+
+optimizer_g = torch.optim.Adam(model.parameters(), 1e-4)
+optimizer_d = torch.optim.Adam(discriminator.parameters(), lr=5e-4)
+# -
 
 # ## Model Training
 
+# It takes ~1hr to train the model
+
 # +
 kl_weight = 1e-6
-optimizer = torch.optim.Adam(model.parameters(), 1e-5)
-n_epochs = 50
+n_epochs = 100
 val_interval = 10
-epoch_loss_list = []
-val_epoch_loss_list = []
+autoencoder_warm_up_n_epochs = 10
+
+epoch_recon_loss_list = []
+epoch_gen_loss_list = []
+epoch_disc_loss_list = []
+val_recon_epoch_loss_list = []
 intermediary_images = []
 n_example_images = 4
 
 for epoch in range(n_epochs):
     model.train()
+    discriminator.train()
     epoch_loss = 0
-    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), ncols=70)
+    gen_epoch_loss = 0
+    disc_epoch_loss = 0
+    progress_bar = tqdm(enumerate(train_loader), total=len(train_loader), ncols=110)
     progress_bar.set_description(f"Epoch {epoch}")
     for step, batch in progress_bar:
         images = batch["image"].to(device)
-        optimizer.zero_grad(set_to_none=True)
+        optimizer_g.zero_grad(set_to_none=True)
+
         reconstruction, z_mu, z_sigma = model(images)
 
-        l1_loss = F.l1_loss(reconstruction.float(), images.float())
-
+        recons_loss = F.l1_loss(reconstruction.float(), images.float())
+        p_loss = perceptual_loss(reconstruction.float(), images.float())
         kl_loss = 0.5 * torch.sum(z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2)) - 1, dim=[1, 2, 3])
         kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+        loss_g = recons_loss + (kl_weight * kl_loss) + (perceptual_weight * p_loss)
 
-        # TODO: Add adversarial component
-        # TODO: Add perceptual loss
+        if epoch > autoencoder_warm_up_n_epochs:
+            logits_fake = discriminator(reconstruction.contiguous().float())[-1]
+            generator_loss = adv_loss(logits_fake, target_is_real=True, for_discriminator=False)
+            loss_g += adv_weight * generator_loss
 
-        loss = l1_loss + kl_weight * kl_loss
-        loss.backward()
-        optimizer.step()
-        epoch_loss += loss.item()
+        loss_g.backward()
+        optimizer_g.step()
+
+        if epoch > autoencoder_warm_up_n_epochs:
+            optimizer_d.zero_grad(set_to_none=True)
+
+            logits_fake = discriminator(reconstruction.contiguous().detach())[-1]
+            loss_d_fake = adv_loss(logits_fake, target_is_real=False, for_discriminator=True)
+            logits_real = discriminator(images.contiguous().detach())[-1]
+            loss_d_real = adv_loss(logits_real, target_is_real=True, for_discriminator=True)
+            discriminator_loss = (loss_d_fake + loss_d_real) * 0.5
+
+            loss_d = adv_weight * discriminator_loss
+
+            loss_d.backward()
+            optimizer_d.step()
+
+        epoch_loss += recons_loss.item()
+        if epoch > autoencoder_warm_up_n_epochs:
+            gen_epoch_loss += generator_loss.item()
+            disc_epoch_loss += discriminator_loss.item()
 
         progress_bar.set_postfix(
             {
-                "loss": epoch_loss / (step + 1),
+                "recons_loss": epoch_loss / (step + 1),
+                "gen_loss": gen_epoch_loss / (step + 1),
+                "disc_loss": disc_epoch_loss / (step + 1),
             }
         )
-    epoch_loss_list.append(epoch_loss / (step + 1))
+    epoch_recon_loss_list.append(epoch_loss / (step + 1))
+    epoch_gen_loss_list.append(gen_epoch_loss / (step + 1))
+    epoch_disc_loss_list.append(disc_epoch_loss / (step + 1))
 
     if (epoch + 1) % val_interval == 0:
         model.eval()
@@ -173,27 +240,20 @@ for epoch in range(n_epochs):
         with torch.no_grad():
             for val_step, batch in enumerate(val_loader, start=1):
                 images = batch["image"].to(device)
-                optimizer.zero_grad(set_to_none=True)
+                optimizer_g.zero_grad(set_to_none=True)
+
                 reconstruction, z_mu, z_sigma = model(images)
-                # get the first sammple from the first validation batch for visualisation
+                # Get the first sammple from the first validation batch for visualisation
                 # purposes
                 if val_step == 1:
                     intermediary_images.append(reconstruction[:n_example_images, 0])
 
-                l1_loss = F.l1_loss(reconstruction.float(), images.float())
+                recons_loss = F.l1_loss(images.float(), reconstruction.float())
 
-                kl_loss = 0.5 * torch.sum(z_mu.pow(2) + z_sigma.pow(2) - torch.log(z_sigma.pow(2)) - 1, dim=[1, 2, 3])
-                kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
-
-                # TODO: Add adversarial component
-                # TODO: Add perceptual loss
-
-                loss = l1_loss + kl_weight * kl_loss
-                val_loss += loss.item()
+                val_loss += recons_loss.item()
 
         val_loss /= val_step
-        val_epoch_loss_list.append(val_loss)
-
+        val_recon_epoch_loss_list.append(val_loss)
         print(f"epoch {epoch + 1} val loss: {val_loss:.4f}")
 progress_bar.close()
 
@@ -204,8 +264,8 @@ progress_bar.close()
 
 plt.figure()
 val_samples = np.linspace(val_interval, n_epochs, int(n_epochs / val_interval))
-plt.plot(np.linspace(1, n_epochs, n_epochs), epoch_loss_list, label="Train")
-plt.plot(val_samples, val_epoch_loss_list, label="Validation")
+plt.plot(np.linspace(1, n_epochs, n_epochs), epoch_recon_loss_list, label="Train")
+plt.plot(val_samples, val_recon_epoch_loss_list, label="Validation")
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
 plt.legend()
@@ -215,11 +275,27 @@ plt.close()
 
 # ### Visualise some reconstruction images
 
-# Plot every evaluation as a new line and example as columns
-fig, ax = plt.subplots(nrows=len(val_samples), ncols=1, sharey=True)
-for image_n in range(len(val_samples)):
+# Get the first 5 examples to plot
+n_evaluations = 5
+fig, ax = plt.subplots(nrows=n_evaluations, ncols=1, sharey=True)
+for image_n in range(n_evaluations):
     reconstructions = torch.reshape(intermediary_images[image_n], (image_size * n_example_images, image_size)).T
     ax[image_n].imshow(reconstructions.cpu(), cmap="gray")
     ax[image_n].set_xticks([])
     ax[image_n].set_yticks([])
     ax[image_n].set_ylabel(f"Epoch {val_samples[image_n]:.0f}")
+
+img = check_data["image"][0]
+fig, ax = plt.subplots(nrows=1, ncols=2)
+ax[0].imshow(check_data["image"][0, 0, ...], vmin=0, vmax=1, cmap="gray")
+ax[0].axis("off")
+ax[0].title.set_text("Inputted Image")
+ax[1].imshow(reconstruction[0, 0, ...].detach().cpu(), vmin=0, vmax=1, cmap="gray")
+ax[1].axis("off")
+ax[1].title.set_text("Reconstruction")
+plt.show()
+
+# ### Clean-up data directory
+
+if directory is None:
+    shutil.rmtree(root_dir)
