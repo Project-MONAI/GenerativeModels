64c62d7b122fabf0011dc37ed0e7f603f7107cd9
Mark Graham <markgraham539@gmail.com>
2022-12-05 14:34:17 -0600

    Calculates mean/var from epsilon

--- generative/inferers/inferer.py
+++ generative/inferers/inferer.py
@@ -12,7 +12,6 @@
 
 from typing import Callable, List, Optional, Tuple, Union
 
-import numpy as np
 import torch
 import torch.nn as nn
 from monai.inferers import Inferer
@@ -107,6 +106,7 @@ class DiffusionInferer(Inferer):
         inputs: torch.Tensor,
         diffusion_model: Callable[..., torch.Tensor],
         scheduler: Optional[Callable[..., torch.Tensor]] = None,
+        predict_epsilon: bool = True,
         save_intermediates: Optional[bool] = False,
         conditioning: Optional[torch.Tensor] = None,
         verbose: Optional[bool] = True,
@@ -118,7 +118,9 @@ class DiffusionInferer(Inferer):
             inputs: input images, NxCxHxW[xD]
             diffusion_model: model to compute likelihood from
             scheduler: diffusion scheduler. If none provided will use the class attribute scheduler
-            save_intermediates: save the
+                        predict_epsilon: flag to use when model predicts the samples directly instead of the noise, epsilon.
+
+            save_intermediates: save the intermediate spatial KL maps
             conditioning:
             verbose: if true, prints the progression bar of the sampling process.
         """
@@ -136,20 +138,49 @@ class DiffusionInferer(Inferer):
             progress_bar = iter(scheduler.timesteps)
         intermediates = []
         noise = torch.randn_like(inputs).to(inputs.device)
-        total_kl = torch.zeros_like(inputs)
+        total_kl = torch.zeros((inputs.shape[0])).to(inputs.device)
         for t in progress_bar:
             timesteps = torch.full(inputs.shape[:1], t, device=inputs.device).long()
             noisy_image = self.scheduler.add_noise(original_samples=inputs, noise=noise, timesteps=timesteps)
             model_output = diffusion_model(x=noisy_image, timesteps=timesteps, context=conditioning)
-            # get the model's predicted mean and variance if it is predicted
+            # get the model's predicted mean,  and variance if it is predicted
             if model_output.shape[1] == inputs.shape[1] * 2 and scheduler.variance_type in ["learned", "learned_range"]:
-                predicted_mean, predicted_variance = torch.split(model_output, inputs.shape[1], dim=1)
+                model_output, predicted_variance = torch.split(model_output, inputs.shape[1], dim=1)
             else:
-                predicted_mean = model_output
                 predicted_variance = None
+
+            # 1. compute alphas, betas
+            alpha_prod_t = scheduler.alphas_cumprod[t]
+            alpha_prod_t_prev = scheduler.alphas_cumprod[t - 1] if t > 0 else scheduler.one
+            beta_prod_t = 1 - alpha_prod_t
+            beta_prod_t_prev = 1 - alpha_prod_t_prev
+
+            # 2. compute predicted original sample from predicted noise also called
+            # "predicted x_0" of formula (15) from https://arxiv.org/pdf/2006.11239.pdf
+            if predict_epsilon:
+                pred_original_sample = (model_output - beta_prod_t ** (0.5) * model_output) / alpha_prod_t ** (0.5)
+            else:
+                pred_original_sample = model_output
+
+            # 3. Clip "predicted x_0"
+            if scheduler.clip_sample:
+                pred_original_sample = torch.clamp(pred_original_sample, -1, 1)
+
+            # 4. Compute coefficients for pred_original_sample x_0 and current sample x_t
+            # See formula (7) from https://arxiv.org/pdf/2006.11239.pdf
+            pred_original_sample_coeff = (alpha_prod_t_prev ** (0.5) * scheduler.betas[t]) / beta_prod_t
+            current_sample_coeff = scheduler.alphas[t] ** (0.5) * beta_prod_t_prev / beta_prod_t
+
+            # 5. Compute predicted previous sample µ_t
+            # See formula (7) from https://arxiv.org/pdf/2006.11239.pdf
+            predicted_mean = pred_original_sample_coeff * pred_original_sample + current_sample_coeff * noisy_image
+
             # get the posterior mean and variance
             posterior_mean = scheduler._get_mean(timestep=t, x_0=inputs, x_t=noisy_image)
             posterior_variance = scheduler._get_variance(timestep=t, predicted_variance=predicted_variance)
+            # at t=0 variance is 0 and the log-variance blows up, fix this
+            if t == 0:
+                posterior_variance = torch.Tensor([1]).to(posterior_mean.device)
             log_posterior_variance = torch.log(posterior_variance)
             log_predicted_variance = torch.log(predicted_variance) if predicted_variance else log_posterior_variance
 
@@ -161,15 +192,14 @@ class DiffusionInferer(Inferer):
                 + torch.exp(log_posterior_variance - log_predicted_variance)
                 + ((posterior_mean - predicted_mean) ** 2) * torch.exp(-log_predicted_variance)
             )
-            total_kl += kl
+            total_kl += kl.view(kl.shape[0], -1).mean(axis=1)
             if save_intermediates:
                 intermediates.append(kl.cpu())
-        total_kl = total_kl.view(total_kl.shape[0], -1).sum(axis=1)
-        log_likelihood_per_dim = -total_kl / np.prod(inputs.shape[1:])
+
         if save_intermediates:
-            return log_likelihood_per_dim, intermediates
+            return total_kl, intermediates
         else:
-            return log_likelihood_per_dim
+            return total_kl
 
 
 class LatentDiffusionInferer(DiffusionInferer):
