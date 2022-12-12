6151176cdc99c51fe95b172af0ce541e361ebe22
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2022-12-12 12:57:26 +0000

    Add class conditioning (#140)
    * Add class conditioning ans tests
    
    Signed-off-by: Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
    
    * Add misssing test (#140)
    
    Signed-off-by: Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
    
    Signed-off-by: Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>

--- tests/test_diffusion_model_unet.py
+++ tests/test_diffusion_model_unet.py
@@ -168,6 +168,44 @@ class TestDiffusionModelUNet2D(unittest.TestCase):
                 norm_num_groups=8,
             )
 
+    def test_shape_conditioned_models_class_conditioning(self):
+        net = DiffusionModelUNet(
+            spatial_dims=2,
+            in_channels=1,
+            out_channels=1,
+            num_res_blocks=1,
+            num_channels=(8, 8, 8),
+            attention_levels=(False, False, True),
+            norm_num_groups=8,
+            num_head_channels=8,
+            num_class_embeds=2,
+        )
+        with eval_mode(net):
+            result = net.forward(
+                x=torch.rand((1, 1, 16, 32)),
+                timesteps=torch.randint(0, 1000, (1,)).long(),
+                class_labels=torch.randint(0, 2, (1,)).long(),
+            )
+            self.assertEqual(result.shape, (1, 1, 16, 32))
+
+    def test_conditioned_models_no_class_labels(self):
+        with self.assertRaises(ValueError):
+            net = DiffusionModelUNet(
+                spatial_dims=2,
+                in_channels=1,
+                out_channels=1,
+                num_res_blocks=1,
+                num_channels=(8, 8, 8),
+                attention_levels=(False, False, True),
+                norm_num_groups=8,
+                num_head_channels=8,
+                num_class_embeds=2,
+            )
+            net.forward(
+                x=torch.rand((1, 1, 16, 32)),
+                timesteps=torch.randint(0, 1000, (1,)).long(),
+            )
+
     def test_script_unconditioned_2d_models(self):
         net = DiffusionModelUNet(
             spatial_dims=2,
