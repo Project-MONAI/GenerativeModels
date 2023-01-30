2f8d7ce4456ec052faedea05e846de017b537ebf
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2023-01-30 15:42:37 +0000

    Add changes to the ipynb files
    Signed-off-by: Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>

--- tutorials/generative/2d_ddpm/2d_ddpm_tutorial_ignite.py
+++ tutorials/generative/2d_ddpm/2d_ddpm_tutorial_ignite.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.1
+#       jupytext_version: 1.14.4
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
--- tutorials/generative/3d_autoencoderkl/3d_autoencoderkl_tutorial.py
+++ tutorials/generative/3d_autoencoderkl/3d_autoencoderkl_tutorial.py
@@ -1,3 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
 # # 3D AutoencoderKL
 
 # This demo is a toy example of how to use MONAI's AutoencoderKL. In particular, it uses the Autoencoder with a Kullback-Leibler regularisation as implemented by Rombach et. al [1].
