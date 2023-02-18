4357bc8fa2ef036e981a5ad2fd8ade3aa246f7dc
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2023-02-18 01:59:05 +0000

    Update model-zoo/models/mednist_ddpm/bundle/scripts/__init__.py
    Co-authored-by: Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>

--- model-zoo/models/mednist_ddpm/bundle/scripts/__init__.py
+++ model-zoo/models/mednist_ddpm/bundle/scripts/__init__.py
@@ -51,6 +51,7 @@ class DiffusionPrepareBatch(PrepareBatch):
 def inv_metric_cmp_fn(current_metric: float, prev_best: float) -> bool:
     """
     This inverts comparison for those metrics which reduce like loss values, such that the lower one is better.
+
     Args:
         current_metric: metric value of current round computation.
         prev_best: the best metric value of previous rounds to compare with.
