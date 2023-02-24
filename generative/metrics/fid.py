972a7d330db4ad17ab8e6ecde3758cc715ded76a
JessyD <jejedafflon@gmail.com>
2023-02-24 17:41:35 -0500

    Compute FID using numpy

--- generative/metrics/fid.py
+++ generative/metrics/fid.py
@@ -31,6 +31,9 @@
 
 from __future__ import annotations
 
+import numpy as np
+from scipy import linalg
+
 import torch
 from monai.metrics.metric import Metric
 
@@ -40,7 +43,7 @@ class FID(Metric):
     Frechet Inception Distance (FID). The FID calculates the distance between two distributions of feature vectors.
     Based on: Heusel M. et al. "Gans trained by a two time-scale update rule converge to a local nash equilibrium."
     https://arxiv.org/abs/1706.08500#. The inputs for this metric should be two groups of feature vectors (with format
-    (number images, number of features)) extracted from the a pretrained network.
+    (number images, number of features)) extracted from a pretrained network.
 
     Originally, it was proposed to use the activations of the pool_3 layer of an Inception v3 pretrained with Imagenet.
     However, others networks pretrained on medical datasets can be used as well (for example, RadImageNwt for 2D and
@@ -56,91 +59,61 @@ class FID(Metric):
 
 
 def get_fid_score(y_pred: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    y = y.float()
-    y_pred = y_pred.float()
 
     if y.ndimension() > 2:
         raise ValueError("Inputs should have (number images, number of features) shape.")
 
-    mu_y_pred = torch.mean(y_pred, dim=0)
-    sigma_y_pred = _cov(y_pred, rowvar=False)
-    mu_y = torch.mean(y, dim=0)
-    sigma_y = _cov(y, rowvar=False)
+    # transform the ys into np arrays
+    y = y.cpu().numpy()
+    y_pred = y_pred.cpu().numpy()
+
+    mu_y_pred = np.mean(y_pred, axis=0)
+    sigma_y_pred = np.cov(y_pred, rowvar=False)
+    mu_y = np.mean(y, axis=0)
+    sigma_y = np.cov(y, rowvar=False)
 
     return compute_frechet_distance(mu_y_pred, sigma_y_pred, mu_y, sigma_y)
 
 
-def _cov(m: torch.Tensor, rowvar: bool = True) -> torch.Tensor:
-    """
-    Estimate a covariance matrix of the variables.
-
-    Args:
-        m: A 1-D or 2-D array containing multiple variables and observations. Each row of `m` represents a variable,
-            and each column a single observation of all those variables.
-        rowvar: If rowvar is True (default), then each row represents a variable, with observations in the columns.
-            Otherwise, the relationship is transposed: each column represents a variable, while the rows contain
-            observations.
-    """
-    if m.dim() < 2:
-        m = m.view(1, -1)
-
-    if not rowvar and m.size(0) != 1:
-        m = m.t()
-
-    fact = 1.0 / (m.size(1) - 1)
-    m = m - torch.mean(m, dim=1, keepdim=True)
-    mt = m.t()
-    return fact * m.matmul(mt).squeeze()
-
-
-def _sqrtm_newton_schulz(matrix: torch.Tensor, num_iters: int = 100) -> tuple[torch.Tensor, torch.Tensor]:
-    """
-    Square root of matrix using Newton-Schulz Iterative method. Based on:
-    https://github.com/msubhransu/matrix-sqrt/blob/master/matrix_sqrt.py. Bechmark shown in:
-    https://github.com/photosynthesis-team/piq/issues/190#issuecomment-742039303
-
-    Args:
-        matrix: matrix or batch of matrices
-        num_iters: Number of iteration of the method
-
-    """
-    dim = matrix.size(0)
-    norm_of_matrix = matrix.norm(p="fro")
-    y_matrix = matrix.div(norm_of_matrix)
-    i_matrix = torch.eye(dim, dim, device=matrix.device, dtype=matrix.dtype)
-    z_matrix = torch.eye(dim, dim, device=matrix.device, dtype=matrix.dtype)
-
-    s_matrix = torch.empty_like(matrix)
-    error = torch.empty(1, device=matrix.device, dtype=matrix.dtype)
-
-    for _ in range(num_iters):
-        t = 0.5 * (3.0 * i_matrix - z_matrix.mm(y_matrix))
-        y_matrix = y_matrix.mm(t)
-        z_matrix = t.mm(z_matrix)
-
-        s_matrix = y_matrix * torch.sqrt(norm_of_matrix)
-
-        norm_of_matrix = torch.norm(matrix)
-        error = matrix - torch.mm(s_matrix, s_matrix)
-        error = torch.norm(error) / norm_of_matrix
-
-        if torch.isclose(error, torch.tensor([0.0], device=error.device, dtype=error.dtype), atol=1e-5):
-            break
-
-    return s_matrix, error
-
-
 def compute_frechet_distance(
-    mu_x: torch.Tensor, sigma_x: torch.Tensor, mu_y: torch.Tensor, sigma_y: torch.Tensor, epsilon: float = 1e-6
-) -> torch.Tensor:
-    """The Frechet distance between multivariate normal distributions."""
+    mu_x: np.ndarray, sigma_x: np.ndarray, mu_y: np.ndarray, sigma_y: np.ndarray, epsilon: float = 1e-6
+) -> np.float:
+    """The Frechet distance between multivariate normal distributions.
+    This implementation is based on https://github.com/mseitzer/pytorch-fid/blob/0a754fb8e66021700478fd365b79c2eaa316e31b/src/pytorch_fid/fid_score.py
+     """
+
+    mu_x = np.atleast_1d(mu_x)
+    mu_y = np.atleast_1d(mu_y)
+
+    sigma_x = np.atleast_2d(sigma_x)
+    sigma_y = np.atleast_2d(sigma_y)
+
+    assert mu_x.shape == mu_y.shape, \
+        'Synthetic and real mean vectors have different lengths'
+    assert sigma_x.shape == sigma_y.shape, \
+        'Synthetic and real covariances have different dimensions'
+
     diff = mu_x - mu_y
-    covmean, _ = _sqrtm_newton_schulz(sigma_x.mm(sigma_y))
 
-    # If calculation produces singular product, epsilon is added to diagonal of cov estimates
-    if not torch.isfinite(covmean).all():
-        offset = torch.eye(sigma_x.size(0), device=mu_x.device, dtype=mu_x.dtype) * epsilon
-        covmean, _ = _sqrtm_newton_schulz((sigma_x + offset).mm(sigma_y + offset))
+    # Product might be almost singular
+    import pdb
+    pdb.set_trace()
+    covmean, _ = linalg.sqrtm(sigma_x.dot(sigma_y), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % epsilon
+        print(msg)
+        offset = np.eye(sigma_x.shape[0]) * epsilon
+        covmean = linalg.sqrtm((sigma_x + offset).dot(sigma_y + offset))
 
-    tr_covmean = torch.trace(covmean)
-    return diff.dot(diff) + torch.trace(sigma_x) + torch.trace(sigma_y) - 2 * tr_covmean
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError(f'Imaginary component {m}')
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma_x)
+            + np.trace(sigma_y) - 2 * tr_covmean)
