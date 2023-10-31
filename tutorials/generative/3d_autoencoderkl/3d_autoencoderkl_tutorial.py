bcdae945e45330329abc6e8938987df197e81ec9
Mark Graham <markgraham539@gmail.com>
2023-10-31 10:33:42 +0000

    Updates notebooks

--- tutorials/generative/3d_autoencoderkl/3d_autoencoderkl_tutorial.py
+++ tutorials/generative/3d_autoencoderkl/3d_autoencoderkl_tutorial.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.14.4
+#       jupytext_version: 1.14.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
--- tutorials/generative/3d_ddpm/3d_ddpm_tutorial.py
+++ tutorials/generative/3d_ddpm/3d_ddpm_tutorial.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.14.4
+#       jupytext_version: 1.14.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
